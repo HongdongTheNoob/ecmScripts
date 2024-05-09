b3d5c77fea2fbd2c81af2df71fdb7a152c3c2995
Qin Hongdong <hongdongdonald@gmail.com>
2024-05-09 15:25:42 +0800

    convert split results

--- ConvertResultsToCSV.py
+++ ConvertResultsToCSV.py
@@ -5,8 +5,8 @@ import re
 import pandas as pd
 import numpy as np
 
-split_tasks = []
-split_task_files = []
+# split_tasks = []
+# split_task_files = []
 
 sequence_names = ['Tango2', 'FoodMarket4', 'Campfire', 
                   'CatRobot', 'DaylightRoad2', 'ParkRunning3',
@@ -17,6 +17,74 @@ sequence_names = ['Tango2', 'FoodMarket4', 'Campfire',
                   'BasketballDrillText', 'ArenaOfValor', 'SlideEditing', 'SlideShow',
                   'FlyingGraphics_420', 'Desktop_420', 'Console_420', 'ChineseEditing_420']
 
+qps = ['22', '27', '32', '37']
+
+video_sequences = {
+  "A1": ["Tango2", "FoodMarket4", "Campfire"],
+  "A2": ["CatRobot", "DaylightRoad2", "ParkRunning3"],
+  "B": ["MarketPlace", "RitualDance", "Cactus", "BasketballDrive", "BQTerrace"],
+  "C": ["BasketballDrill", "BQMall", "PartyScene", "RaceHorsesC"], 
+  "D": ["BasketballPass", "BlowingBubbles", "BQSquare", "RaceHorses"],
+  "E": ["FourPeople", "Johnny", "KristenAndSara"],
+  "F": ["BasketballDrillText", "ArenaOfValor", "SlideEditing", "SlideShow"],
+  "TGM": ["FlyingGraphic", "Desktop", "Console", "ChineseEditing"]
+}
+
+video_frame_rates = {
+  "Tango2": 60,
+  "FoodMarket4": 60,
+  "Campfire": 30,
+  "CatRobot": 60,
+  "DaylightRoad2": 60,
+  "ParkRunning3": 50,
+  "MarketPlace": 60,
+  "RitualDance": 60,
+  "Cactus": 50,
+  "BasketballDrive": 50,
+  "BQTerrace": 60, 
+  "BasketballDrill": 50,
+  "BQMall": 60, 
+  "PartyScene": 50,
+  "RaceHorsesC": 30,
+  "BasketballPass": 50,
+  "BQSquare": 60,
+  "BlowingBubbles": 50,
+  "RaceHorses": 30,
+  "FourPeople": 60,
+  "Johnny": 60,
+  "KristenAndSara": 60,
+  "BasketballDrillText": 50,
+  "ArenaOfValor": 60,
+  "SlideEditing": 30,
+  "SlideShow": 20,
+  "FlyingGraphics_420": 60,
+  "Desktop_420": 60,
+  "Console_420": 60,
+  "ChineseEditing_420": 60
+}
+
+def extract_after_last_ra(string):
+    index = string.rfind("-RA-")
+    if index != -1:
+        return string[index + len("-RA-"):]
+    else:
+        return None  # Handle case when "-RA-" is not found
+
+def count_integers(string):
+    # Split the string based on non-numeric characters
+    parts = string.split("-")
+
+    # Count the number of parts that are integers
+    count = sum(1 for part in parts if part.isdigit())
+
+    return count
+
+def sort_frame_number(item):
+    # Find all integers at the end of the string
+    integers = re.findall(r'\d+$', item)
+    # Convert the last integer to an integer and return it
+    return int(integers[-1]) if integers else 0
+    
 def csv_file_reordering(df):
   new_df = pd.DataFrame(columns=df.columns)
   for _ in range(480):
@@ -57,9 +125,13 @@ def csv_file_reordering(df):
 def search_files(directory, output_file):
   with open(output_file, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['File Path', 'Frame Count', '', 'Bit Rate', 'Y-PSNR', 'U-PSNR', 'V-PSNR', 'Encode Time', 'Decode Time', 'VM Peak'])
+    # writer.writerow(['File Path', 'Frame Count', '', 'Bit Rate', 'Y-PSNR', 'U-PSNR', 'V-PSNR', 'Encode Time', 'Decode Time', 'VM Peak'])
     for root, dirs, files in os.walk(directory):
       for file in files:
+        # check file names, exclude split files
+        if "-RA-" in file:
+          if count_integers(extract_after_last_ra(file)) > 1:
+            continue
         if file.endswith('.txt') and ~file.endswith('decode.txt'):
           file_path = os.path.relpath(os.path.join(root, file), directory)
           file_name_with_extension = os.path.basename(file_path)
@@ -72,10 +144,10 @@ def search_files(directory, output_file):
             num_integers = len(re.findall(r'\d+', matched_part))
             # Skip splitted sequences
             if num_integers > 1:
-              split_task_files.append(file_path)
-              task = file_name.rsplit('-', 1)[0]
-              if task not in split_tasks:
-                split_tasks.append(task)
+              # split_task_files.append(file_path)
+              # task = file_name.rsplit('-', 1)[0]
+              # if task not in split_tasks:
+              #   split_tasks.append(task)
               continue
           with open(os.path.join(root, file), 'r') as txtfile:
             found_line = False
@@ -107,18 +179,65 @@ def search_files(directory, output_file):
   # df = df.sort_values(by=['File Path Upper'], ascending = True)
   # del df['File Path Upper']
   # df.to_csv(output_file, index = False)
-  df = pd.read_csv(output_file, header=0)
-  new_df = csv_file_reordering(df)
-  new_df.to_csv(output_file, index = False, header = False)
 
-# def search_split_files(output_file):
-#   with open(output_file, 'a', newline='') as csvfile:
-#     for task in split_tasks:
+def search_split_files(directory, output_file):
+  with open(output_file, 'a', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    for video_class in video_sequences.keys():
+      for sequence in video_sequences[video_class]:
+        video_directory = os.path.join(directory, "Class" + video_class, sequence)
+        for qp in qps:
+          split_log_files = []
+          for root, dirs, files in os.walk(video_directory):
+            for file in files:
+              if ~file.endswith('.txt'):
+                continue
+              if file.endswith('decode.txt'):
+                continue
+              if "-RA-" not in file:
+                continue
+              qp_and_frame_number = extract_after_last_ra(file)
+              if qp != qp_and_frame_number.split('-')[0]:
+                continue
+              if count_integers(qp_and_frame_number) < 2:
+                continue
+              split_log_files.append(os.path.join(root, file))
+          
+          # rank
+          split_log_files = sorted(split_log_files, key = sort_frame_number)
+
+          frame_count = 0
+          total_bits = 0
+          y_PSNR_sum = 0.0
+          u_PSNR_sum = 0.0
+          v_PSNR_sum = 0.0
+          for i in len(split_log_files):
+            with open(split_log_files[i], 'r') as txtfile:
+              found_first = False
+              for line in txtfile:
+                match_bits = re.search(r"(\d+) bits")
+                match_psnr = re.search(r"xY (\w{16}) xU (\w{16}) xV (\w{16})", line)
+                if match_bits and match_psnr:
+                  if (not found_first) and i > 0: # discard first frame
+                    found_first = True
+                    continue
+                  total_bits += int(match_bits.group(1))
+                  xY_hex = match_psnr.group(1)
+                  xU_hex = match_psnr.group(2)
+                  xV_hex = match_psnr.group(3)
+                  y_PSNR_sum += np.float64(int(xY_hex, 16))
+                  u_PSNR_sum += np.float64(int(xU_hex, 16))
+                  v_PSNR_sum += np.float64(int(xV_hex, 16))
+                  frame_count += 1
+
+          task_string = os.path.join("Class" + video_class, sequence, "log-" + sequence + "-RA-" + qp + ".txt")
+          row_values = [task_string, '', str(float(total_bits)/(frame_count * video_frame_rates[sequence])), str(y_PSNR_sum/frame_count), str(u_PSNR_sum/frame_count), str(v_PSNR_sum/frame_count), '', '']
+      
       
 def search_files_decode(directory, output_file):
   with open(output_file, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['File Path', 'Decode Time', '', 'VM Peak'])
+    # writer.writerow(['File Path', 'Decode Time', '', 'VM Peak'])
     for root, dirs, files in os.walk(directory):
       for file in files:
         if file.endswith('decode.txt'):
@@ -168,7 +287,11 @@ if __name__ == '__main__':
   # single coded files
   search_files(input_directory, output_file)
   # split coded files
-  # search_split_files(output_file)
+  search_split_files(input_directory, output_file)
+  # reorder everything
+  df = pd.read_csv(output_file, header=0)
+  new_df = csv_file_reordering(df)
+  new_df.to_csv(output_file, index = False, header = False)
 
   if convert_decode_file:
     output_file_decode = output_file.replace(".csv", "-decode.csv")
